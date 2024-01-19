8afc78ccb63b68b2e3273b85b2af586ede29640c
brendanmcc02 <brendan.mccann02@gmail.com>
2024-01-19 13:34:08 +0000

    changed directory structure; removed notes.txt

---
+++ scripts/updateDB.sh
@@ -0,0 +1,6 @@
+cd /home/brendanmcc/Desktop/film-data-vis/
+node updateDB.js
+git add data/filmData.json
+git add data/metadata.json
+git commit -m "ran updateDB.js"
+git push
--- updateDB.sh
+++
@@ -1,5 +0,0 @@
-cd /home/brendanmcc/Desktop/film-data-vis/
-node updateDB.js
-git add .
-git commit -m "ran updateDB script"
-git push
