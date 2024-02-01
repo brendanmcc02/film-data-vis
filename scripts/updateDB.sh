0aba9e69842ba8f7cded4d1f4b295d89ffd80fe8
brendanmcc02 <brendan.mccann02@gmail.com>
2024-02-01 14:46:40 +0000

    changed directory updateDB.sh

--- scripts/updateDB.sh
+++ scripts/updateDB.sh
@@ -1,6 +1,6 @@
 # 1. runs updateDB.js
 # 2. commits & pushes changes to filmData.json & metadata.json
-cd /home/brendanmcc/Desktop/film-data-vis/src/ || exit
+cd /home/brendanmcc02/Desktop/film-data-vis/src/ || exit
 node updateDB.js
 cd ..
 git add data/filmData.json
