4ae10b9bd8859f7960958e205f40ef4443341992
brendanmcc02 <brendan.mccann02@gmail.com>
2024-02-18 16:11:27 +0000

    blah

--- src/test.js
+++ src/test.js
@@ -1,38 +1,38 @@
-import nodeFetch from "node-fetch";
-import * as fs from "fs";
-main();
-
-async function main() {
-    let filmData = readFromJson("../data/filmData.json");
-
-    filmData.forEach(film => {
-       delete film.runtime;
-    });
-
-    writeToJson(filmData, "../data/filmData.json");
-}
-
-// reads .json file into a variable
-function readFromJson(filepath) {
-    let filmData;
-
-    try {
-        filmData = fs.readFileSync(filepath);
-    } catch (error) {
-        throw error;
-    }
-
-    return JSON.parse(filmData);
-}
-
-
-// writes data to a .json file
-function writeToJson(data, filepath) {
-    const stringData = JSON.stringify(data, null, 4);
-
-    fs.writeFileSync(filepath, stringData, (error) => {
-        if (error) {
-            throw error;
-        }
-    });
-}
+// import nodeFetch from "node-fetch";
+// import * as fs from "fs";
+// main();
+//
+// async function main() {
+//     let filmData = readFromJson("../data/filmData.json");
+//
+//     filmData.forEach(film => {
+//        delete film.runtime;
+//     });
+//
+//     writeToJson(filmData, "../data/filmData.json");
+// }
+//
+// // reads .json file into a variable
+// function readFromJson(filepath) {
+//     let filmData;
+//
+//     try {
+//         filmData = fs.readFileSync(filepath);
+//     } catch (error) {
+//         throw error;
+//     }
+//
+//     return JSON.parse(filmData);
+// }
+//
+//
+// // writes data to a .json file
+// function writeToJson(data, filepath) {
+//     const stringData = JSON.stringify(data, null, 4);
+//
+//     fs.writeFileSync(filepath, stringData, (error) => {
+//         if (error) {
+//             throw error;
+//         }
+//     });
+// }
