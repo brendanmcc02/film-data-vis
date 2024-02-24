3fcacd293256fef7dba67c7d29e085a565f814c5
brendanmcc02 <brendan.mccann02@gmail.com>
2024-02-24 16:12:43 +0000

    removed .idea/ remotely

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,7 @@
-# run a web server on port 8080
+# install node dependencies
 cd ..
+npm install
+# run a web server on port 8080
 cd web/ || exit
 echo "Go to localhost:8080/web/index.html"
 http-server -p 8080
